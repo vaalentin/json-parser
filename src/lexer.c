51d4b72f2c26020044e4ce4235c50d43f6f72574
Valentin Marmonier <valentin.marmonier@gmail.com>
2014-04-14 18:53:37 +0800

    from if else if to switch statement

--- src/lexer.c
+++ src/lexer.c
@@ -55,66 +55,51 @@ void lex(char path[]) {
 
 			currentChar = fgetc(file);
 
-			/**
-			 * match the current character
-			 * 
-			 * Json reserved characters : { } [ ] " , :
-			 * but only outside of " "
-			 * that's why we use quotationOpen as a switch to know if we should capture them
-			 */
-			if(currentChar == '\n') {
-				currentLine++;
-				currentCol = 0;
-			}
-			else if(currentChar == '{') {
-				appendToken("LEFT_BRACKET", NULL, currentLine, currentCol, currentCol);
-			}
-			else if(currentChar == '}') {
-				appendToken("RIGHT_BRACKET", NULL, currentLine, currentCol, currentCol);
-			}
-			else if(currentChar == '[') {
-				appendToken("LEFT_SQUARE", NULL, currentLine, currentCol, currentCol);
-			}
-			else if(currentChar == ']') {
-				appendToken("RIGHT_SQUARE", NULL, currentLine, currentCol, currentCol);
-			}
-			else if(currentChar == '"') {
-				// start the buffering
-				quotationOpen = !quotationOpen;
-				// we just found a closing "
-				if(!quotationOpen) {
-
-					// capture the buffer content
-					// add 1 to have enough space to put '\0'
-					char text[buffer.used + 1];
-					for(int i = 0; i < buffer.used; i++) {
-						text[i] = buffer.array[i];
-						text[i + 1] = '\0';
+			switch(currentChar) {
+				case '\n':
+					currentLine++;
+					currentCol = 0;
+					break;
+				case '{':
+					appendToken("LEFT_BRACKET", NULL, currentLine, currentCol, currentCol);
+					break;
+				case '}':
+					appendToken("RIGHT_BRACKET", NULL, currentLine, currentCol, currentCol);
+					break;
+				case '[':
+					appendToken("LEFT_SQUARE", NULL, currentLine, currentCol, currentCol);
+					break;
+				case ']':
+					appendToken("RIGHT_SQUARE", NULL, currentLine, currentCol, currentCol);
+					break;
+				case ':':
+					appendToken("ASSIGNATION", NULL, currentLine, currentCol, currentCol);
+					break;
+				case ',':
+					appendToken("COMA", NULL, currentLine, currentCol, currentCol);
+					break;
+				case '"':
+					quotationOpen = !quotationOpen; // start the buffering
+					if(!quotationOpen) { // finish the buffering
+						// capture the buffer content
+						char text[buffer.used + 1];
+						for(int i = 0; i < buffer.used; i++) {
+							text[i] = buffer.array[i];
+							text[i + 1] = '\0';
+						}
+						emptyBuffer(&buffer); // empty the buffer
+						appendToken("VALUE", text, currentLine, start, currentCol);
+					} else {
+						start = currentCol + 1; // otherwise, a word to capture started
 					}
-
-					appendToken("VALUE", text, currentLine, start, currentCol);
-
-					// empty the buffer
-					emptyBuffer(&buffer);
-
-				} else {
-					// otherwise, a word to capture started
-					start = currentCol + 1;
-				}
-
-				appendToken("QUOTE", NULL, currentLine, currentCol, currentCol);
-			}
-			else if(currentChar == ':') {
-				appendToken("ASSIGNATION", NULL, currentLine, currentCol, currentCol);
-			}
-			else if(currentChar == ',') {
-				appendToken("COMA", NULL, currentLine, currentCol, currentCol);
-			}
-			else {
-				// not a reserved character, append it to the buffer
-				if(quotationOpen) {
-					appendBuffer(&buffer, currentChar);
-				}
+					appendToken("QUOTE", NULL, currentLine, currentCol, currentCol);
+					break;
+				default:
+					// not a reserved character, append it to the buffer
+					if(quotationOpen) {
+						appendBuffer(&buffer, currentChar);
+					}
+					break;
 			}
 
 			currentCol++;
