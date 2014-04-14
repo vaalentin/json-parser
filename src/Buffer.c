78d74b622c4837b3efa2fb06ea7846d58eaea175
Valentin Marmonier <valentin.marmonier@gmail.com>
2014-04-14 13:48:04 +0800

    clean and comment

--- src/Buffer.c
+++ src/Buffer.c
@@ -1,14 +1,21 @@
+/**
+ * \file Buffer.c
+ * \brief Buffer implementation
+ * \version 0.0.1
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include "Buffer.h"
 
 void initBuffer(Buffer *buffer, size_t size) {
+	// by using malloc, we will be able to use realloc
 	buffer->array = (int *)malloc(size * sizeof(char));
 	buffer->size = size;
 	buffer->used = 0;
 }
 
 void appendBuffer(Buffer *buffer, char character) {
+	// if we reach the max size, * 2
 	if(buffer->used == buffer->size) {
 		buffer->size *= 2;
 		buffer->array = (int *)realloc(buffer->array, buffer->size * sizeof(char));
@@ -17,6 +24,7 @@ void appendBuffer(Buffer *buffer, char character) {
 }
 
 void emptyBuffer(Buffer *buffer) {
+	// '\0' close the array
 	buffer->array[0] = '\0';
 	buffer->used = 0;
 	buffer->size = 0;
